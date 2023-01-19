aa1fd37bedac6758ac42b26631ec9d1a141ad705
Vergel Linsangan <40718012+vergell@users.noreply.github.com>
2023-01-19 18:08:51 +0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,85 +1,70 @@
-let l;
-let c = 0;
-let lines = [];
-let space;
-let pos;
-let strWeight;
-let arrLength;
-let scnLength;
-let dropdown;
-let randomIndex;
-let bubbleSortBttn, runBubbleSort;
-let shuffleBttn, runSuffle;
-let bar;
-
+let center, space;
+let lines;
+let slider, shuffleButton, bubbleSortButton;
+let arrLength, randomIndex, scanLength;
+let runSuffle, runBubbleSort;
+j;
 function setup() {
-  shuffleBttn = createButton("shuffle");
-  shuffleBttn.mousePressed(shufflePrssd);
+  createCanvas(windowWidth, 400);
+
+  lines = [];
   runSuffle = false;
 
-  bubbleSortBttn = createButton("bubble sort ");
-  bubbleSortBttn.mousePressed(bubbleSortPrssd);
+  slider = select("#test5");
+  slider.input(createLines);
+
+  shuffleButton = select("#shuffles");
+  shuffleButton.mousePressed(shufflePrssd);
+
+  bubbleSortButton = select("#bubble");
+  bubbleSortButton.mousePressed(bubbleSortPrssd);
   runBubbleSort = false;
 
-  //select colors
-  dropdown = createSelect();
-  dropdown.option("orange");
-  dropdown.option("green");
-  dropdown.option("skyblue");
+  space = 2;
+  center = windowWidth / 2;
+  frameRate(5);
+  createLines();
 
-  //select how many bars
-  bar = createSlider(10, 100, 40);
-  bar.input(updateBar);
+  arrLength = lines.length;
+  scanLength = 0;
+}
 
-  let canvas = createCanvas(windowWidth, 400);
-  l = 56;
-  space = 8;
-  strWeight = 5;
-
-  pos = width / 2 - l * (strWeight - 1);
-  for (let a = 0; a < l; a++) {
+function createLines() {
+  lines = [];
+  for (let a = 0; a < slider.value(); a++) {
     lines.push(
-      new Lines((pos += space), height, random(height - 90), strWeight)
+      new Lines(
+        a * 12 + (center - (12 * slider.value()) / 2),
+        400,
+        random(400 - 90),
+        8
+      )
     );
   }
+
   arrLength = lines.length;
   scanLenght = 0;
 }
 
-function updateBar() {
-  lines = [];
-  pos = width / 2 - bar.value() * (strWeight - 1);
-  for (let a = 0; a < bar.value(); a++) {
-    lines.push(
-      new Lines((pos += space), height, random(height - 90), strWeight)
-    );
-  }
-
-  arrLength = lines.length;
-}
 function shufflePrssd() {
   runSuffle = !runSuffle;
   arrLength = lines.length;
 }
+
 function bubbleSortPrssd() {
   runBubbleSort = !runBubbleSort;
 }
 
 function draw() {
-  background(dropdown.value());
-
+  background(255);
   for (each of lines) {
-    each.render(255, 0, 0);
+    each.render(0, 131, 143);
   }
-
-  print(arrLength);
-
-  if (runBubbleSort) {
-    bubbleSort();
-  }
-
   if (runSuffle) {
     shuffleff();
+  }
+  if (runBubbleSort) {
+    bubbleSort();
   }
 }
 
@@ -92,12 +77,8 @@ function shuffleff() {
     swap(arrLength, randomIndex);
     if (arrLength <= 0) {
       arrLength = lines.length;
-
       runSuffle = false;
     }
-    // temp = lines[currentIndex];
-    // lines[currentIndex] = lines[randomIndex];
-    // lines[randomIndex] = temporaryValue;
   }
 }
 
@@ -108,19 +89,20 @@ function bubbleSort() {
     runBubbleSort = false;
   }
   if (arrLength >= 0) {
-    if (scanLenght >= arrLength - 1) {
+    if (scanLength >= arrLength - 1) {
       arrLength--;
-      scanLenght = 0;
+      scanLength = 0;
     } else {
-      lines[scanLenght].render(0, 0, 255);
-      lines[scanLenght + 1].render(0, 255, 0);
-      if (lines[scanLenght].valY() > lines[scanLenght + +1].valY()) {
-        swap(scanLenght, scanLenght + 1);
+      lines[scanLength].render(0, 0, 255);
+      lines[scanLength + 1].render(0, 255, 0);
+      if (lines[scanLength].valY() > lines[scanLength + +1].valY()) {
+        swap(scanLength, scanLength + 1);
       }
-      scanLenght++;
+      scanLength++;
     }
   }
 }
+
 function swap(a, b) {
   let temp = lines[a].valY();
   lines[a].change(lines[b].valY());
